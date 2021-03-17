c8eba9ec4e326f99f2f919f33488ad38770eda98
vonGorski <62354549+vonGorski@users.noreply.github.com>
2021-03-17 12:57:10 +0100

    we

--- FlowShop/DATA.h
+++ FlowShop/DATA.h
@@ -14,6 +14,9 @@ public:
 	int P;
 	int Q;
 	int RPQ;
+	int RP;
+	int PQ;
+	//int RQ
 	int lp;
 
 	DATA(int n,int r,int p,int q ) {
@@ -22,6 +25,9 @@ public:
 		Q = q;
 		lp = n;
 		RPQ = R + P + Q;
+		RP = R + P;
+		PQ = P + Q;
+		//RQ = R / Q;
 			}
 
 	void PRINT()
@@ -30,6 +36,8 @@ public:
 			 " " << Q << "   RPQ = " 
 			 << RPQ << endl;
 	}
+
+
 
 	bool R_less_than_Q()
 	{
@@ -41,10 +49,12 @@ public:
 	bool operator > (const DATA str) const
 	{
 		return (RPQ > str.RPQ);
+		//return (RQ > str.RQ);
 	}
 	bool operator < (const DATA str) const
 	{
 		return (RPQ < str.RPQ);
+		//return (RQ > str.RQ);
 	}
 
 };
--- FlowShop/FlowShop.cpp
+++ FlowShop/FlowShop.cpp
@@ -42,16 +42,102 @@ void read_from_file(int I, vector<DATA>& data)
  
  }
 
+
+ int cmax(vector<DATA> data)
+ {
+     int m = 0, c = 0;
+     for (int i = 0; i < data.size(); i++)
+     {
+         m = max(m, data[i].R) + data[i].P;
+         c = max(c, m + data[i].Q);
+
+     }
+     return c;
+ }
+
+ void auto_sort_R(vector<DATA>& data)
+ {
+    for(int j=0; j<data.size()-1; j++)
+    {
+        for (int i = 0; i < data.size() - j - 1; i++) {
+            if (data[i].R < data[i + 1].R)
+            {
+                DATA temp = data[i + 1];
+                data[i + 1] = data[i];
+                data[i] = temp;
+
+            }
+        }
+     }
+ }
+
+ void auto_sort_RP(vector<DATA>& data)
+ {
+     for (int j = 0; j < data.size() - 1; j++)
+     {
+         for (int i = 0; i < data.size() - j - 1; i++) {
+             if (data[i].RP < data[i + 1].RP)
+             {
+                 DATA temp = data[i + 1];
+                 data[i + 1] = data[i];
+                 data[i] = temp;
+
+             }
+         }
+     }
+ }
+
+ void auto_sort_PQ(vector<DATA>& data)
+ {
+     for (int j = 0; j < data.size() - 1; j++)
+     {
+         for (int i = 0; i < data.size() - j - 1; i++) {
+             if (data[i].PQ < data[i + 1].PQ)
+             {
+                 DATA temp = data[i + 1];
+                 data[i + 1] = data[i];
+                 data[i] = temp;
+
+             }
+         }
+     }
+
+ }
+
+ void auto_sort_Q(vector<DATA>& data)
+ {
+     for (int j = 0; j < data.size() - 1; j++)
+     {
+         for (int i = 0; i < data.size() - j - 1; i++) {
+             if (data[i].Q < data[i + 1].Q)
+             {
+                 DATA temp = data[i + 1];
+                 data[i + 1] = data[i];
+                 data[i] = temp;
+
+             }
+         }
+     }
+ 
+ }
+
+
  void my_algoritm(vector<DATA> raw_data, vector<DATA>& data)
  {
      // wrzucam nawjiekszy na srodek
      data.push_back(raw_data[raw_data.size()-1]);
-     for (int i = 1; i<raw_data.size(); i++)
+     for (int i = raw_data.size()-1; i>=0; i-- )
      {
-         if (raw_data[i].R_less_than_Q() == true) data.insert(data.begin(), 1, raw_data[i]);
-         else data.push_back(raw_data[i]);
+         vector<DATA> temp;
+         temp = data;
+         temp.insert(temp.begin(), 1, raw_data[i]);
+         data.push_back(raw_data[i]);
+         if (cmax(data) >> cmax(temp))  data = temp;
      }
   }
+
+
+
 
 
 
@@ -60,12 +146,37 @@ int main()
    vector<DATA> raw_data[4];
    vector<DATA> data[4];
 
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < 2; i++) { //ma byc<4
         read_from_file(i, raw_data[i]);
-        print_from_vector(raw_data[i]);
+      //  print_from_vector(raw_data[i]);
         sort_one_vector(raw_data[i]);
-        my_algoritm(raw_data[i], data[i]);
-        cout << " PO sortowaniu i smiesznym algorytmie << endl;" << endl;
-        print_from_vector(data[i]);
+        cout << " PO sortowaniu RPQ" << endl << endl;
+        print_from_vector(raw_data[i]);
+        cout << cmax(raw_data[i]);
+       // my_algoritm(raw_data[i], data[i]);
+        auto_sort_R(raw_data[i]);
+        cout << " PO sortowaniu R "<< endl << endl;
+        print_from_vector(raw_data[i]);
+        cout << cmax(raw_data[i]) << endl;
+
+        auto_sort_RP(raw_data[i]);
+        cout << " PO sortowaniu RP" << endl << endl;
+        print_from_vector(raw_data[i]);
+        cout << cmax(raw_data[i]) << endl;
+
+        auto_sort_PQ(raw_data[i]);
+        cout << " PO sortowaniu PQ" << endl << endl;
+        print_from_vector(raw_data[i]);
+        cout << cmax(raw_data[i]) << endl;
+
+        auto_sort_Q(raw_data[i]);
+        cout << " PO sortowaniu Q" << endl << endl;
+        print_from_vector(raw_data[i]);
+        cout << cmax(raw_data[i]) << endl;
+      //  cout << " PO sortowaniu i smiesznym algorytmie << endl;" << endl;
+      //  print_from_vector(data[i]);
+      //  cout << cmax(data[i]);
     }
+
+    
 }
